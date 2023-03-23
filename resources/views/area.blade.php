5f9c78e8e41fedc659bbf3c41a06e5bfbaff48b8
Jose Mora <jdmds13@gmail.com>
2023-03-23 09:59:33 -0400

    antes de la base de datos

---
+++ resources/views/area.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('contenido')
+    <div class="tituloPrincipal">
+        <h1>Area</h1>
+    </div> 
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Jd Page - @yield('titulo')</title>
+</head>
+<body>
+    <div class="header">
+        <h1>Jd</h1>
+        <div class="lista-Area">
+            
+        </div>
+    </div>
+@yield('contenido')
+   
+    
+</body>
+</html>
